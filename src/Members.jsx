fe317e6cfe95fdb69c4b58862b8bc246639b63b8
Marvelio <umarvel87@gmail.com>
2025-12-04 03:05:35 +0700

    Added CSS Grid styling and responsiveness member cards

---
+++ src/Members.jsx
@@ -0,0 +1,10 @@
+// src/components/Members/Members.jsx
+
+import React from 'react';
+import './Members.css'; // Pastikan baris ini ada!
+
+const Members = () => {
+  // ... kode komponen Anda
+};
+
+export default Members;
